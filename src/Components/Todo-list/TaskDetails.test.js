aff06c4f907c3005e2a9285935b93b79ea6d8d87
Rokan Sikder <rokan08@gmail.com~>
2022-02-09 22:24:10 -0500

    Fixed package.json

--- src/Components/Todo-list/TaskDetails.test.js
+++ src/Components/Todo-list/TaskDetails.test.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import {render, screen} from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import TaskDetails from './TaskDetails';
 
-const todoTask = {id: 2, taskName: 'Project 2', isCompleted: false};
+const todoTask = { id: 2, taskName: 'Project 2', isCompleted: false };
 
 test('renders task details', () => {
-    const {getByText} = render(<TaskDetails task={todoTask} />);
+    const { getByText } = render(<TaskDetails task={todoTask} />);
     expect(getByText(/Project 2/i)).toBeInTheDocument();
 });
 
